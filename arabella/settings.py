e46b15ae449831aecb3d6b8f373654ee320efd58
hamzakataf <129067152+hamzakataf@users.noreply.github.com>
2025-12-24 15:54:27 +0300

    Update ALLOWED_HOSTS to allow all hosts

--- arabella/settings.py
+++ arabella/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x7lpyubb97iq%b7i@z(x-tkmk^)8k7vvlmy%-!6((*@cxefsi+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
